a295d96746fe7113780c3e49320d194b91faa829
Jose Ramon Vieites <jose.vieites@altia.es>
2022-03-18 03:58:36 +0100

    Clase 6

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,15 @@
 <script>
   let someText = `Frontend Developer at Platzi`;
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <div class="About">
   <p>{someText}</p>
+  <button on:click={handleClick}> Click {count === 0 ? '' : count}</button>
 </div>
 
 <style>
