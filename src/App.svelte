e97e1609f08597744ace9da15374baf98690634b
Jose Ramon Vieites <jose.vieites@altia.es>
2022-03-18 03:45:50 +0100

    Clase 4

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,50 @@
 <script>
-	export let name;
-	export let lastName;
+  export let name;
+  export let lastName;
+  let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
 </script>
 
 <main>
-	<h1>Hello {name} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name} {lastName}!</h1>
+  <p>Frontend Developer</p>
+  <img src={svelteLogo} alt="Svelte">
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang="scss">
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  :global(:root) {
+    --theme-color: purple;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+  p {
+    color: var(--theme-color);
+    font-size: 22px;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+	img {
+		width: 30%;
 	}
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
